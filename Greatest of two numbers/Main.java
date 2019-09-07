715daaf9d8e6beedb95f3b88969332e9d71afa7b
Akhand Pratap Singh <48331690+akhandgithub@users.noreply.github.com>
2019-09-07 18:00:11 +0530

    Test code pushed to repository.

---
+++ Greatest of two numbers/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+      int Num1 = in.nextInt();
+      int Num2 = in.nextInt();
+      if(Num1 > Num2)
+        System.out.println("num1 is the greatest number");
+      else
+        System.out.println("num2 is the greatest number");
+	}
+}
